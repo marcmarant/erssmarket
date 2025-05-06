10c43a70c1591c0cd8ac5b87bc2f09500d1e7662
marcmarant <marcosmardev@gmail.com>
2025-05-06 20:43:31 +0200

    login funcionando

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 from .routes import init_routes
 from .db import init_db
@@ -9,8 +10,13 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
-    # Configuracion del secreto de JWT
+    # Configuracion de JWT
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config['JWT_TOKEN_LOCATION'] = ['headers']
+    app.config['JWT_HEADER_NAME'] = 'Authorization'
+    app.config['JWT_HEADER_TYPE'] = 'Bearer'
+    
+    jwt = JWTManager(app)
 
     init_db(app)
     init_routes(app)
--- app/api/autenticacion.py
+++ app/api/autenticacion.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import create_access_token, jwt_required
+from flask_jwt_extended import create_access_token
 from ..db import db
 from ..db import Usuario
 
 autenticacion = Blueprint('autenticacion', __name__)
 # TODO : Borrar a futuro usuarios de prueba a falta de DB
 users = [
-  Usuario(1, "Juan Pérez", "juan.perez@example.com", "pass123", "2025-04-30T12:34:56"),
-  Usuario(2, "Ana López", "ana.lopez@example.com", "asv134", "2025-04-30T13:45:12")
+  Usuario(1, 'Juan Pérez', 'juan.perez@example.com', 'pass123', '2025-04-30T12:34:56'),
+  Usuario(2, 'Ana López', 'ana.lopez@example.com', 'asv134', '2025-04-30T13:45:12')
 ]
 
 """
@@ -15,15 +15,13 @@ Ruta para el login del usuario, recibe email y contraseña
 y devuelve un token JWT si las credenciales son correctas.
 """
 @autenticacion.route('/login', methods=['POST'])
-@jwt_required()
 def login():
-  print("Iniciando sesión")
   data = request.json
   email = data.get('email')
-  contrasenya = data.get('contrasenya')
+  password = data.get('password')
 
-  if not email or not contrasenya:
-    return jsonify({"error": "Email y contraseña son requeridos"}), 400
+  if not email or not password:
+    return jsonify({'error': 'Los campos email y contraseña son necesarios'}), 400
 
   #user = Usuario.query.filter_by(email=email).first()
   user = next((u for u in users if u.email == email), None) # Simulando la consulta a la base de datos
@@ -32,7 +30,7 @@ def login():
     return jsonify({"error": "Usuario o Contraseña incorrecta"}), 400
 
   # check password
-  if user.contrasenya != contrasenya:
+  if user.contrasenya != password:
     return jsonify({"error": "Usuario o Contraseña incorrecta"}), 400
 
   access_token = create_access_token(identity={"id": user.id, "nombre": user.nombre, "email": user.email})
