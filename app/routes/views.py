190a82bd150fcb80a3e502fc67ec0807079ec78b
marcmarant <marcosmardev@gmail.com>
2025-05-06 21:16:17 +0200

    login casi hecho

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 main = Blueprint('main', __name__)
 
@@ -6,8 +7,17 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('home.html');
 
+"""
+Ruta con la vista para iniciar sesión en la aplicación
+Si el usuario ya está loggeado se le redirige a home
+"""
 @main.route('/login')
+@jwt_required(optional=True)
 def login():
+    current_user = get_jwt_identity()
+    print(current_user)
+    if current_user:
+        return redirect(url_for('main.home'))
     return render_template('login.html');
 
 @main.route('/selector')
