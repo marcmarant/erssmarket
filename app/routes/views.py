666471a04a1a2d2cff82bad927952066e0d6b842
marcmarant <marcosmardev@gmail.com>
2025-05-07 20:37:30 +0200

    add base

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, redirect
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 main = Blueprint('main', __name__)
@@ -6,6 +6,10 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('home.html');
+
+@main.route('/home2')
+def home2():
+    return render_template('home2.html');
 
 """
 Ruta con la vista para iniciar sesión en la aplicación
@@ -17,7 +21,6 @@ def login():
     current_user = get_jwt_identity()
     print(current_user)
     if current_user:
-        print("Usuario loggeado")
         return redirect(url_for('main.home'))
     return render_template('login.html');
 
