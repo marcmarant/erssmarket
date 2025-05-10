c654cce5e441ef06f4911f192cdd3c19422a6cb9
belmeji <belidmejialargo@gmail.com>
2025-05-10 19:55:49 +0200

    Más cambios

--- app/api/carrito.py
+++ app/api/carrito.py
@@ -7,6 +7,8 @@ carrito = Blueprint('carrito', __name__)
 
 @carrito.route('/agregar', methods=['POST'])
 def agregar_al_carrito():
+
+  printf("Entro a agregar producto en el carrito")
   try:
     data = request.get_json()
     usuario_id = data.get('usuario_id')
@@ -16,16 +18,22 @@ def agregar_al_carrito():
     if (cantidad <= 0):
       return jsonify({'error': 'cantidad incorrecta'})
 
-      producto = Carrito.query.filter_by(usuario_id=usuario_id, producto_id=producto_id).first()
+    """
+    #Cuando haya bd se busca si actualemnte hay un carrito asignado a este usuario
+    carrito_item = Carrito.query.filter_by(usuario_id=usuario_id, producto_id=producto_id).first()
 
-      if producto:
-        producto.cantidad += cantidad
-      else:
-        producto = Carrito(usuario_id=usuario_id, producto_id=producto_id, cantidad=cantidad)
-        db.session.add(producto)
+    if carrito_item:
+        # Si ya existe, aumentar la cantidad
+        carrito_item.cantidad += cantidad
+    else:
+        # Si no existe, crear una nueva entrada en el carrito
+        carrito_item = Carrito(usuario_id=usuario_id, producto_id=producto_id, cantidad=cantidad)
+        db.session.add(carrito_item)
 
-      db.session.commit()
-      return jsonify({'message': f'Producto añadido al carrito correctamente. Cantidad: {cantidad}'}), 201
+    db.session.commit()
+    """
+    print("Producto añadido al carrito")
+    return jsonify({'message': f'Producto añadido al carrito correctamente. Cantidad: {cantidad}'}), 201
   except Exception as e:
     db.session.rollback()
     return jsonify({"error": str(e)}), 500
