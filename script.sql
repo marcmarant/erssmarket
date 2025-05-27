75531d5e516fab3ad3af09a5fca49e700f0bd3c1
marcmarant <marcosmardev@gmail.com>
2025-05-27 13:06:46 +0200

    add inserts

--- script.sql
+++ script.sql
@@ -74,3 +74,16 @@ VALUES
 ('Camiseta Deportiva', 'Camiseta deportiva ligera y cómoda, ideal para entrenamientos y actividades físicas. Fabricada con tejido transpirable que mantiene la piel fresca y seca. Su diseño clásico y ajuste regular garantizan libertad de movimiento. Perfecta para el día a día o el gimnasio.', 25, '../static/images/camiseta.jpg', 1),
 ('Pantalón Deportivo', 'Pantalones deportivos cómodos y ligeros, perfectos para entrenar o relajarse. Fabricados con tejido transpirable que absorbe la humedad, mantienen la frescura en cada movimiento. Su corte ajustado pero flexible garantiza libertad y confort. Ideales para el gimnasio o el uso diario.', 30, '../static/images/pantalon.jpg', 1),
 ('Zapatillas Deportivas', 'Zapatillas deportivas versátiles y cómodas, diseñadas para entrenamientos y uso diario. Cuentan con una suela antideslizante que ofrece tracción y estabilidad. Su plantilla acolchada garantiza amortiguación en cada paso. Perfectas para mantener el ritmo en cualquier actividad.', 50, '../static/images/zapatilla.jpg', 1);
+
+INSERT INTO pedido (usuario_id, precio_total) 
+VALUES 
+(1, 45), -- Pedido 1 de Juan Pérez
+(2, 95); -- Pedido 2 de Juan Pérez
+
+INSERT INTO producto_pedido (pedido_id, producto_id, precio, cantidad) 
+VALUES 
+(1, 1, 25, 1),
+(1, 2, 20, 1),
+(2, 2, 15, 3),
+(2, 3, 30, 2),
+(2, 4, 50, 1);
