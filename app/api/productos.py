62fbd64fb2f656f4c8d13a72b6bc47338b75ca98
marcmarant <marcosmardev@gmail.com>
2025-05-31 01:41:03 +0200

    fix como antes

--- app/api/productos.py
+++ app/api/productos.py
@@ -45,12 +45,12 @@ def update_product(id):
         product = Producto.query.filter(Producto.id == id).first()
         if not product:
             return jsonify({"error": "No existe ningún producto con el id dado"}), 404
-        if 'nombre' not in data and 'descripcion' not in data and 'precio' not in data and 'stock' not in data:
+        if 'nombre' not in data or 'descripcion' not in data or 'precio' not in data or 'stock' not in data:
             return jsonify({"error": "Peticion Invalida se necesita nombre, descripción, precio y stock"}), 400
         if not isinstance(data['precio'], (int)):
-                return jsonify({"error": "El precio debe ser un entero en centimos"}), 400
+            return jsonify({"error": "El precio debe ser un entero en centimos"}), 400
         if not isinstance(data['stock'], (int)):
-                return jsonify({"error": "El stock debe ser un entero"}), 400
+            return jsonify({"error": "El stock debe ser un entero"}), 400
         product.nombre = data['nombre']
         product.descripcion = data['descripcion']
         product.precio = data['precio']
